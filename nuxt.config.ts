35f6fc849af23f7bb589b34eb1adfaf2fd90f099
Owen Hathaway <owen@prsdata.com>
2023-11-15 21:17:53 -0700

    massive progress on migrating vue3 version, still problems with sub-pages, icons, and FormKit theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  css: [
+    'bootstrap/scss/bootstrap.scss',
+    '@fortawesome/fontawesome-svg-core/styles.css',
+    '~/assets/scss/site.scss'
+  ],
+  devtools: { enabled: true },
+  formkit: { autoImport: true },
+  modules: [
+    '@formkit/nuxt',
+    '@pinia/nuxt'
+  ],
+  pages: true
 })
